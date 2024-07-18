24688341f480a2e5f5329fa8ad7d93d796869af2
Edon <edon@valdman.works>
2024-07-18 17:58:19 -0500

    Create SessionAuthenticationMiddleware.swift
    Created new types using `OpenAPISecuritySchemes` to use the spec's security scheme.

---
+++ Sources/SelfServerRESTTypes/SessionAuthenticationMiddleware.swift
@@ -0,0 +1,30 @@
+//
+//  SessionAuthenticationMiddleware.swift
+//
+//
+//  Created by Edon Valdman on 7/17/24.
+//
+
+import OpenAPIRuntime
+import Foundation
+import HTTPTypes
+
+import OpenAPISecuritySchemes
+
+/// A client middleware that injects a value into the `Authorization` header field of the request.
+package struct SessionTokenSecurityScheme: BearerHTTPSecurityScheme {
+    package static let bearerFormat: HTTPSecuritySchemeName.BearerFormat? = .jwt
+    
+    package let accessToken: String
+    
+    package init(_ sessionToken: String) {
+        self.accessToken = sessionToken
+    }
+}
+
+package struct SessionTokenMiddleware: SecuritySchemeMiddleware {
+    package weak var delegate: (any SecuritySchemeMiddlewareDelegate)? = nil
+    
+    package init() {
+    }
+}
