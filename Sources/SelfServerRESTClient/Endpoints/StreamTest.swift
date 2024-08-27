ff99fd5178e9c54e6687fc48fc27323b398c4fd5
Edon <edon@valdman.works>
2024-08-27 14:15:32 -0500

    Update StreamTest.swift
    Implemented initial basic use of `assetComplete` part.

--- Sources/SelfServerRESTClient/Endpoints/StreamTest.swift
+++ Sources/SelfServerRESTClient/Endpoints/StreamTest.swift
@@ -12,27 +12,46 @@ import OpenAPIRuntime
 import SelfServerRESTTypes
 import SelfServerTypes
 
+public enum FileTransferData: Hashable, Sendable {
+    case assetChunk(Data, assetName: String, range: ClosedRange<Int>)
+    case assetComplete(assetName: String, assetSize: Int64)
+}
+
 extension SelfServerRESTClient {
     public func streamTest<S: AsyncSequence>(
         libraryID: UUID,
         transferID: UUID,
         assetDataStream: S
-    ) async throws where S.Element == Data, S: Sendable {
+    ) async throws where S.Element == FileTransferData, S: Sendable {
         let mappedStream = assetDataStream
             .map { data -> Components.RequestBodies.AssetTransfer.multipartFormPayload in
-                return .asset_chunk(
-                    .init(
-                        payload: .init(
-                            headers: .init(
-                                X_hyphen_Asset_hyphen_Name: "test",
-                                // TODO: use new HTTPRanges package
-                                Range: "bytes=0-"
-                            ),
-                            body: .init(data)
-                        ),
-                        filename: "Test File Name"
+                switch data {
+                case .assetChunk(let chunk, let assetName, let range):
+                    return .asset_chunk(
+                        .init(
+                            payload: .init(
+                                headers: .init(
+                                    X_hyphen_Asset_hyphen_Name: assetName,
+                                    Range: "bytes=\(range.lowerBound)-\(range.upperBound)"
+                                ),
+                                body: .init(chunk)
+                            )
+                        )
                     )
-                )
+                    
+                case .assetComplete(let assetName, let assetSize):
+                    return .asset_complete(
+                        .init(
+                            payload: .init(
+                                headers: .init(
+                                    X_hyphen_Asset_hyphen_Name: assetName,
+                                    Content_hyphen_Length: Int(assetSize)
+                                ),
+                                body: .init("complete")
+                            )
+                        )
+                    )
+                }
             }
         
         let output = try await self._client.fileUploadTest(
