5cac2694c52eaecfa0c2d3ae5f947c954a7c4310
Edon <edon@valdman.works>
2024-08-28 15:06:38 -0500

    Updated dependency version

--- Package.swift
+++ Package.swift
@@ -22,10 +22,7 @@ let package = Package(
         .package(url: "https://github.com/swift-server/swift-openapi-async-http-client", from: "1.0.0"),
         
         // MARK: Self-Serve
-        .package(
-            url: "https://github.com/SelfServeApp/self-server-openapi-swift.git",
-            branch: "main"
-        ),
+        .package(url: "https://github.com/SelfServeApp/self-server-openapi-swift.git", from: "0.1.3"),
         .package(url: "https://github.com/SelfServeApp/self-server-extensions.git", from: "0.2.0"),
         
         // MARK: Helper
