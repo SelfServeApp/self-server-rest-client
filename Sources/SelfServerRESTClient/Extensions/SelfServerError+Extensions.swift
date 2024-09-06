091b9b158885ea44ed556b253b3a5d133d6870c7
Edon <edon@valdman.works>
2024-09-06 10:44:01 -0500

    Update SelfServerError+Extensions.swift
    Updated typings to match updated spec.

--- Sources/SelfServerRESTClient/Extensions/SelfServerError+Extensions.swift
+++ Sources/SelfServerRESTClient/Extensions/SelfServerError+Extensions.swift
@@ -12,17 +12,17 @@ import SelfServerRESTTypes
 import NIOHTTP1
 
 extension SelfServerError {
-    internal init(abortError: Components.Responses.AbortError, statusCode: HTTPResponseStatus) {
-        guard let errorCodeInt = abortError.headers.X_hyphen_Self_hyphen_Server_hyphen_Error_hyphen_Code,
+    internal init(errorResponse response: Components.Responses.GeneralError, statusCode: HTTPResponseStatus) {
+        guard let errorCodeInt = response.headers.X_hyphen_Self_hyphen_Server_hyphen_Error_hyphen_Code,
               let errorCode = SelfServerErrorCode(rawValue: UInt(errorCodeInt)) else {
             self.init(statusCode: Int(statusCode.code), payload: .init())
             return
         }
         
-        self.init(abortCode: errorCode, reason: try? abortError.body.json.reason)
+        self.init(abortCode: errorCode, reason: try? response.body.json.reason)
     }
     
-    internal init(abortError: Components.Responses.AbortError, statusCode: Int) {
-        self.init(abortError: abortError, statusCode: .init(statusCode: statusCode))
+    internal init(errorResponse response: Components.Responses.GeneralError, statusCode: Int) {
+        self.init(errorResponse: response, statusCode: .init(statusCode: statusCode))
     }
 }
