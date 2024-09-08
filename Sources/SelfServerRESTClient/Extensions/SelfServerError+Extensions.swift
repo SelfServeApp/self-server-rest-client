ed92c3c082710060a03bd4454fa39b67a1f7e5df
Edon <edon@valdman.works>
2024-09-08 18:01:04 -0500

    Update SelfServerError+Extensions.swift
    Added new init for `400` error response.

--- Sources/SelfServerRESTClient/Extensions/SelfServerError+Extensions.swift
+++ Sources/SelfServerRESTClient/Extensions/SelfServerError+Extensions.swift
@@ -12,6 +12,17 @@ import SelfServerRESTTypes
 import NIOHTTP1
 
 extension SelfServerError {
+    /// A convenience initializer from a `GeneralError` response, which should have a `400`/`Bad Request` status code.
+    internal init(error400 response: Components.Responses.GeneralError) {
+        guard let errorCodeInt = response.headers.X_hyphen_Self_hyphen_Server_hyphen_Error_hyphen_Code,
+              let errorCode = SelfServerErrorCode(rawValue: UInt(errorCodeInt)) else {
+            self.init(statusCode: .badRequest, payload: .init())
+            return
+        }
+        
+        self.init(badRequest400: errorCode, reason: try? response.body.json.reason)
+    }
+    
     /// A convenience initializer from a `401UnauthorizedResponse` response, which should have a `401`/`Unauthorized` status code.
     internal init(error401 response: Components.Responses._401UnauthorizedResponse) {
         self.init(unauthorized401: response.headers.WWW_hyphen_Authenticate)
